a7c3359ccf4e043d27a8a96f8afd88aed84be771
gerrard <gyc_ssdut@163.com>
2020-08-28 21:12:15 +0800

    format

--- cpp/leetcode/62.unique-paths.cpp
+++ cpp/leetcode/62.unique-paths.cpp
@@ -3,33 +3,29 @@
  *
  * [62] Unique Paths
  */
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 // @lc code=start
 
-vector<int>row(101,0);
-vector<vector<int>>dp(101, row);
+vector<int> row(101, 0);
+vector<vector<int>> dp(101, row);
 class Solution {
-public:
+   public:
     int uniquePaths(int m, int n) {
-        if(m <=0 or n <= 0)
-            return 0;
-        if(m==1 or n == 1)
-            return 1;
-        if(dp[m][n] == 0){
-            dp[m][n] = uniquePaths(m-1, n) + uniquePaths(m, n-1);
+        if (m <= 0 or n <= 0) return 0;
+        if (m == 1 or n == 1) return 1;
+        if (dp[m][n] == 0) {
+            dp[m][n] = uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
         }
         return dp[m][n];
     }
 };
 // @lc code=end
 
-
-int main () {
+int main() {
     Solution s;
-    cout<<s.uniquePaths(7, 3)<<endl;
+    cout << s.uniquePaths(7, 3) << endl;
     return 0;
 }
-
